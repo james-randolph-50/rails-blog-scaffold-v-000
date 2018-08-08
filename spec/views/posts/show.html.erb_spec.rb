4b4c338a27b8809e1bef9d77d5f7121aa7852555
James Randolph <james.randolph2016@gmail.com>
2018-08-08 22:25:34 +0000

    automatically backed up by learn

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe "posts/show", type: :view do
   before(:each) do
     @post = assign(:post, Post.create!(
       :title => "Title",
-      :description => "Description"
+      :description => "MyText"
     ))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Title/)
-    expect(rendered).to match(/Description/)
+    expect(rendered).to match(/MyText/)
   end
 end
